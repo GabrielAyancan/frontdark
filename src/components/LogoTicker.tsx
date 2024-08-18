f280f3987985f965b2aac7858e9608f0e69145a5
Gabriel Ayancan <gf130109@gmail.com>
2024-08-18 13:25:00 -0300

    update

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -24,11 +24,11 @@ export const LogoTicker = () => {
         <h2 className="text-xl text-center text-white/70">Trusted by the world's most innovate teams</h2>
         <div className="flex overflow-hidden mt-9 before:content-[''] before:z-10 after:content-[''] before:absolute after:absolute before:h-full after:h-full before:w-5 after:w-5 relative after:right-0 before:left-0 before:top-0 after:top-0 before:bg-[linear-gradient(to_right,#000,rgb(0,0,0,0))] after:bg-[linear-gradient(to_left,#000,rgb(0,0,0,0))]">
           <motion.div transition={{duration: 10, ease:"linear", repeat: Infinity, }} initial={{ translateX: 0}} animate={{ translateX: "-50%"}} className="flex gap-16 flex-none pr-16">
-            {images.map(({ src, alt }) => (
-              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+            {images.map(({ src, alt }, key) => (
+              <Image src={src} alt={alt} className="flex-none h-8 w-auto" key={key}/>
             ))}
-            {images.map(({ src, alt }) => (
-              <Image src={src} alt={alt} className="flex-none h-8 w-auto" />
+            {images.map(({ src, alt }, key) => (
+              <Image src={src} alt={alt} className="flex-none h-8 w-auto" key={key}/>
               ))}
           </motion.div>
         </div>
