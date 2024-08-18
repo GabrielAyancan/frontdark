629b3db7ea05f661d5745559baca612796003045
Gabriel Ayancan <gf130109@gmail.com>
2024-08-18 01:05:52 -0300

    Initial commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
     <footer className="py-5 bg-black text-white/60 border-t border-white/20">
       <div className="container">
         <div className="flex flex-col gap-4 sm:flex-row sm:jusitify-between">
-          <div className="text-center">2024 Your Company, Inc. All rights reserved </div>
+          <div className="text-center">2024 Ayancan, Inc. All rights reserved </div>
           <ul className="flex justify-center gap-2.5">
             <li><XSocial /></li>
             <li><InstaIcon /></li>
